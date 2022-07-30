e1870f3e32c77f24c404b98379915dd910559c34
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2022-07-30 12:33:05 +0200

    chnage deploy options

--- init.sh
+++ init.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 host_user=$(whoami)
 app=$1
+id=$(openssl rand -base64 12)
 secret=$(openssl rand -base64 32)
 
 sed -i "s/{user}/$host_user/g" prod.json # Chage user name to empty string
 sed -i "s/{app}/$app/g" prod.json # Chage user name to empty string
 sed -i "s/{secret}/$secret/g" prod.json # Chage user name to empty string
+sed -i "s/{id}/$id/g" prod.json # Chage user name to empty string
 
 sed -i "s/{user}/$host_user/g" test.json # Chage user name to empty string
 sed -i "s/{app}/$app/g" test.json # Chage user name to empty string
 sed -i "s/{secret}/$secret/g" test.json # Chage user name to empty string
+sed -i "s/{id}/$id/g" test.json # Chage user name to empty string
+
+echo "use this secret on your repository webhook setings \n: $secret"
+echo "your config sectings {id: $id, secret: $secret}"
