b19240a7855462540b93530b9cd4c1969a458568
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2022-07-30 14:30:03 +0200

    add auto naming

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Auto Deploy Configurations
+You ned to provide the name of the application to be deployed 
+
+To generate the configurations, you can use the following command:
+
+```
+$ mkdir -p ~/server/<app_name>
+$ cd <app_name>
+$ git clone git@github.com:ishop-africa/iadeploy.git deploy
+$ cd deploy
+$ ./init.sh <app_name>
+```
--- init.sh
+++ init.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 host_user=$(whoami)
-app=$1
-id=$(openssl rand -base64 12)
-secret=$(openssl rand -base64 32)
+app=$(cd ../ && basename "$PWD")
 
 sed -i "s/{user}/$host_user/g" prod.json # Chage user name to empty string
 sed -i "s/{app}/$app/g" prod.json # Chage user name to empty string
-sed -i "s/{your-secrets-here}/$secret/g" prod.json # Chage user name to empty string
-sed -i "s/{id}/$id/g" prod.json # Chage user name to empty string
+secretProd=$(openssl rand -base64 32)
+sed -i "s/echo-secrets/$secretProd/g" prod.json # Chage user name to empty string
+idProd=$(openssl rand -base64 12)
+sed -i "s/{id}/$idProd/g" prod.json # Chage user name to empty string
 
 sed -i "s/{user}/$host_user/g" test.json # Chage user name to empty string
 sed -i "s/{app}/$app/g" test.json # Chage user name to empty string
-sed -i "s/{your-secrets-here}/$secret/g" test.json # Chage user name to empty string
-sed -i "s/{id}/$id/g" test.json # Chage user name to empty string
+secretTest=$(openssl rand -base64 32)
+sed -i "s/{your-secrets-here}/$secretTest/g" test.json # Chage user name to empty string
+idTest=$(openssl rand -base64 12)
+sed -i "s/{id}/$idTest/g" test.json # Chage user name to empty string
 
 echo "use this secret on your repository webhook setings: $secret"
-echo "your config sectings {id: $id, secret: $secret}"
+echo "your config sectings 
+   {
+    TEST:  {id: $idTest, secret: $secretTest},
+    PROD:{
+        id: $idProd,
+        secret: $secretProd}
+    } 
+    "
