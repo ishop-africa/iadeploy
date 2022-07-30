c3d552c05e109a21433c5b1edeb6a1d6ca3c6736
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2022-07-30 14:14:46 +0200

    updated

--- init.sh
+++ init.sh
@@ -6,12 +6,12 @@ secret=$(openssl rand -base64 32)
 
 sed -i "s/{user}/$host_user/g" prod.json # Chage user name to empty string
 sed -i "s/{app}/$app/g" prod.json # Chage user name to empty string
-sed -i "s/{secret}/$secret/g" prod.json # Chage user name to empty string
+sed -i "s/{your-secrets-here}/$secret/g" prod.json # Chage user name to empty string
 sed -i "s/{id}/$id/g" prod.json # Chage user name to empty string
 
 sed -i "s/{user}/$host_user/g" test.json # Chage user name to empty string
 sed -i "s/{app}/$app/g" test.json # Chage user name to empty string
-sed -i "s/{secret}/$secret/g" test.json # Chage user name to empty string
+sed -i "s/{your-secrets-here}/$secret/g" test.json # Chage user name to empty string
 sed -i "s/{id}/$id/g" test.json # Chage user name to empty string
 
 echo "use this secret on your repository webhook setings: $secret"
